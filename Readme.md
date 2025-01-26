885269e2e0bda789832e95bb25f8de3e995e4b13
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-01-26 17:58:57 +0000

    install

--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # add-vm (3e projet de la branche admisys)
 ## avoir virtualbox
 ## télécharger l'iso de debian
+## instaler debian
--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "debian"
+git commit -m "install"
 git push
